f6cc257e0f18ea859679e0717804888152d24d5f
ChristopherL <anbchris@gmail.com>
2025-11-20 22:30:21 -0500

    add to previous

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -13,6 +13,16 @@ export function withMatcher<
   AC extends (...args: any[]) => AnyAction & { type: string },
 >(actionCreator: AC): Matchable<AC>;
 
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;

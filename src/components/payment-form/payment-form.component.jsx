bd54e5f854561119e6215ba07e88f108cead61da
ChristopherL <anbchris@gmail.com>
2025-12-15 17:51:32 -0500

    changing error handling on payment form

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -50,7 +50,7 @@ export default function PaymentForm() {
     setIsProcessingPayment(false);
 
     if (paymentResult.error) {
-      alert(paymentResult.error);
+      alert(paymentResult.error.message);
     } else {
       if (paymentResult.paymentIntent.status === "succeeded") {
         alert("Payment Successful");
